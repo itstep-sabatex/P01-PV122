f36f7628ac63e8eea46a1ef12ed8a15462877901
sabatex <serhiy.lakas@hotmail.com>
2023-11-13 21:23:57 +0200

    *

---
+++ DLLImportDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.InteropServices;
+using System.Diagnostics;
+namespace DLLImportDemo
+{ 
+   
+    internal static class Program
+    { 
+        [DllImport("user32.dll", CallingConvention = CallingConvention.StdCall)]
+        static extern void MessageBox(IntPtr hWnd, string text, string caption, uint uType = 0x02);
+        static void Main(string[] args)
+        {
+            //  eax - l
+            //  ecx - h
+            //  edx - l -b
+            //  ebx  -h - b
+            //  add eax,edx 67 ff
+            //  addc ecx,ebx
+            //  c - 
+
+            Console.WriteLine("Hello, World!");
+            IntPtr intPtr = Process.GetCurrentProcess().MainWindowHandle;
+            MessageBox(intPtr, "Title dll window", "Demo");
+            Thread.Sleep(5000);
+            
+
+            // ClassName;Name;
+            // Dog;Bobik;5;Gav;brown
+            // Kat;Alf;6;May;red
+
+        }
+    }
+}
---
+++ ThreadDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadDemo
+{
+    internal class Program
+    {
+        static double MultiplreOneElement(int dim, int i, int j, double[,] a, double[,] b)
+        {
+            double result = 0;
+            //var xDim = a.GetLength(0);
+            //var yDim = a.GetLength(1);
+
+            for (int mi = 0; mi < dim; mi++)
+            {
+                result = result + a[i, mi] * b[mi, j];
+            }
+            return result;
+        }
+
+        static void ThreadA(object p)
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine($"Starting thred {p}");
+        }
+
+        static void Main(string[] args)
+        {
+           
+            
+
+            
+            for(int i = 0; i < 100; i++)
+            {
+                int a = i;
+                //Console.WriteLine($"Start Thred {i}!");
+                var tr = new Thread(ThreadA);
+                //tr.IsBackground=true;
+                tr.Start($"Start Thred {i}!");
+            }
+            Thread.Sleep(6000);
+            Console.WriteLine("Program ended");
+        }
+    }
+}
