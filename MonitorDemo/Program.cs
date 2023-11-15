12a23bf15c00980119401bedb47068e90f45ae10
sabatex <serhiy.lakas@hotmail.com>
2023-11-15 21:26:44 +0200

    *

---
+++ MonitorDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int a = 0;
+
+Object locked=new object();
+void Increment(string threadName)
+{
+    lock (locked)
+    {
+        a++;
+        Console.WriteLine($"{threadName} a={a}");
+    }
+    
+}
+
+Task.Run(() =>
+{
+    for (int i = 0; i < 10; i++)
+    {
+            Increment("Task1");
+    }
+});
+
+Task.Run(() =>
+{
+    for (int i = 0; i < 10; i++)
+    {
+            Increment("Task2");
+       
+    }
+});
+
+Thread.Sleep(5000);
