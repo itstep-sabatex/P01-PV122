0dfecdd8d54d3ea05b008259f130e05869fb0d4a
sabatex <serhiy.lakas@hotmail.com>
2023-12-04 22:48:04 +0200

    add tcp/udp client demo

---
+++ TCPDemo/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+using System.Net.Sockets;
+
+// server
+Task.Run(() => 
+{ 
+	TcpListener listener = null;
+	try
+	{
+		int port = 5000;
+		IPAddress localAddress = new IPAddress(new byte[] {127,0,0,1 });
+		listener = new TcpListener(localAddress, port);
+		listener.Start();
+		var buffer = new byte[1024];
+		string data = string.Empty;
+		while (true)
+		{
+			Console.WriteLine("Чекаємо на зєднання...");
+		    TcpClient client = listener.AcceptTcpClient();
+			Console.WriteLine("Client connected ...");
+			data = null;
+			NetworkStream stream = client.GetStream();
+			int i;
+			while ((i=stream.Read(buffer,0,buffer.Length)) != 0)
+			{
+				data = System.Text.Encoding.UTF8.GetString(buffer,0,i);
+				Console.WriteLine("Server Resived: {0}", data);
+				data = data.ToUpper();
+				var msg = System.Text.Encoding.UTF8.GetBytes(data);
+				stream.Write(msg,0,msg.Length);
+				Console.WriteLine("Server Sent: {0}", data);
+
+			}
+			client.Close();
+		}
+	}
+	catch { }
+	finally { listener?.Stop();
+	}
+
+});
+
+Task.Run(() => 
+{
+	try
+	{
+		var client = new TcpClient("127.0.0.1", 5000);
+		string message = "test message";
+		var data = System.Text.Encoding.UTF8.GetBytes(message);
+		var stream = client.GetStream();
+		stream.Write(data, 0, data.Length);
+		Console.WriteLine("Client Send: {0}", message);
+		data = new byte[1024];
+		var responceData = string.Empty;
+		int bytes = stream.Read(data, 0, data.Length);
+		responceData = System.Text.Encoding.UTF8.GetString(data, 0, bytes);
+		Console.WriteLine("Client Received: {0}", responceData);
+		stream.Close();
+		client.Close();
+	}
+	catch { }
+});
+
+Console.ReadLine();
---
+++ UDPDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+UdpClient udpClient = new UdpClient();
+udpClient.Client.Bind(new IPEndPoint(IPAddress.Any, 5000));
+Task.Run(() => {
+	var from = new IPEndPoint(0, 5000);
+	while (true)
+	{
+		Task.Delay(1000).Wait();
+		var recBuffer = udpClient.Receive(ref from);
+		Console.WriteLine($"Recesive: {Encoding.UTF8.GetString(recBuffer,0,recBuffer.Length)}");
+
+	}
+});
+
+
+for (int i = 0; i < 100000; i++)
+{
+	var data = Encoding.UTF8.GetBytes($"Hello World {i}");
+	udpClient.Send(data, data.Length, "255.255.255.255", 5000);
+}
+Console.WriteLine("All packet sending");
+Console.ReadLine();
+
